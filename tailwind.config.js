d5276a1a973c0f3e2733759d2a26dc79c76e7eef
Patryk Smakosz <pas@baernholdt.com>
2022-12-15 15:38:33 +0100

    add: basic fonts, colors, animation

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
+    extend: {
+      fontFamily: {
+        main: ["Melanzane", "sans-serif"],
+        secondary: ["Bodoni 72", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
